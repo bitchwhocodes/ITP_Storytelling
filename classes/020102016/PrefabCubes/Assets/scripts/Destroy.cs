e20462333f4f95dc122013b24d6fbaa60229a9ab
bitchwhocodes <stacey.mulcahy@gmail.com>
2016-02-16 07:04:34 -0800

    last weeks classes

---
+++ classes/020102016/PrefabCubes/Assets/scripts/Destroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 5.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
