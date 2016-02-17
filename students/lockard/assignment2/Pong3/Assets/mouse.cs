3a920efc28d5167bee47155e26f0ee53a9985ee5
David Lockard <David_Lockard@172-16-220-42.DYNAPOOL.NYU.EDU>
2016-02-17 16:48:49 -0500

    2nd assignement David Lockard

---
+++ students/lockard/assignment2/Pong3/Assets/mouse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouse : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float moveSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			mousePosition = Input.mousePosition;
+			mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+		}
+
+	}
+}
