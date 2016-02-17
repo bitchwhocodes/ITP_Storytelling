10c0359f301f7d7259e94f4535fbe53436d6b649
Shan Jin <smalleight17@gmail.com>
2016-02-17 18:31:28 -0500

    space walker first commit

---
+++ students/jin/assignment2/Sensory_storytelling/Assets/Script/Running_Sheep.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Running_Sheep : MonoBehaviour {
+
+	public GameObject prefab_sheep;
+
+	private float radius=3.0f;
+	private Vector3 pluto_pos;
+	private Quaternion rotation;
+	private List<GameObject> sheep_list;
+
+	void Start () {
+		pluto_pos= GetComponent<Transform>().position;
+		rotation = Quaternion.identity;
+		rotation.eulerAngles = new Vector3(90, 0, 0);
+		sheep_list = new List<GameObject> ();
+
+		for (int angle=0; angle<360; angle+=60) {
+			float radians=angle*Mathf.PI/180;
+			Vector3 sheep_pos=new Vector3(pluto_pos.x+radius*Mathf.Cos(radians),pluto_pos.y,pluto_pos.z+radius*Mathf.Sin(radians));
+			//Debug.Log("Cos("+angle+"):"+Mathf.Cos(radians));
+
+			GameObject new_sheep=(GameObject)Instantiate (prefab_sheep,sheep_pos,Quaternion.identity);
+
+			new_sheep.GetComponent<Sheep_script>().original_angle=angle;
+
+			sheep_list.Add(new_sheep);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotate the sheeps
+//		foreach(GameObject sheep in sheep_list){
+//
+//		}
+	}
+
+
+}
---
+++ students/jin/assignment2/Sensory_storytelling/Assets/Script/Spaceship_control.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spaceship_control : MonoBehaviour {
+
+
+	public GameObject bolt;
+	public GameObject smoke;
+
+	private float movementSpeed=10.0F;
+
+	private Transform trans;
+	private Rigidbody rb;
+	private Transform smoke_spawn;
+
+	private float fireRate=0.25f;
+	private float nextFire=0.0f;
+
+	private Vector3 smoke_position;
+	private Quaternion smoke_rotation;
+
+	// Use this for initialization
+	void Start () {
+		trans = GetComponent<Transform> ();
+		rb = GetComponent<Rigidbody> ();
+		smoke_spawn = GetComponent<Transform> ().Find ("smoke_spawn");
+	}
+
+	void Update () {
+
+		//fire
+		if (Input.GetKey (KeyCode.Space) && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			Instantiate(bolt,trans.position,trans.rotation);
+			//Debug.Log("instantiate a bolt");
+		}
+		
+	}
+
+	void FixedUpdate(){
+		//change direction
+		Vector3 rotate_angle = trans.rotation.eulerAngles;
+		rotate_angle.y+=Input.GetAxis ("Horizontal");
+		transform.rotation = Quaternion.Euler (rotate_angle);
+		
+		//go forward 
+		if(Input.GetKey(KeyCode.Return)){
+			rb.AddForce(trans.forward * movementSpeed);
+
+			//eject smoke
+			smoke_rotation=Quaternion.identity;
+			smoke_rotation.eulerAngles=new Vector3(90,trans.rotation.eulerAngles.y,trans.rotation.eulerAngles.z);
+			//smoke_position=new Vector3(trans.position.x,-0.2f,trans.position.z);
+			Instantiate(smoke,smoke_spawn.position,smoke_rotation);
+
+			//Debug.Log(trans.rotation.eulerAngles);
+		}
+
+
+		//trying to simulate resistance, end up using Drag in Rigidbody
+
+		//if (rb.velocity.magnitude>0.1) {
+		//	rb.AddForce (trans.forward* (-0.5f));
+		//}
+		//Debug.Log ("Velocity:"+rb.velocity.magnitude);
+
+	}
+}
